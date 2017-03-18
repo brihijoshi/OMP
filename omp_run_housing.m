fb6f19b6aee0dc757ac7d2a2d12f2cdfddcd61a2
Brihi <brihijoshi08@gmail.com>
2017-03-19 02:25:50 +0530

    First

---
+++ omp_run_housing.m
@@ -0,0 +1,17 @@
+clear all;
+m=dlmread("housing.data");
+sz=size(m,1);
+tr=round(0.7*sz);
+te=sz-tr;
+
+m_tr=m(1:tr,:);
+o_tr=m_tr(:,end);
+m_tr=m_tr(:,1:end-1);
+
+m_te=m(end-te+1:end,:);
+o_te=m_te(:,end);
+m_te=m_te(:,1:end-1);
+
+
+z=omp(m_tr,o_tr,m_te,o_te,5)
+
